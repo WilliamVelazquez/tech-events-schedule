e4b66a01fb834744464f7bb27fbfe8000e520e91
William Velazquez <william.velazquez.a@gmail.com>
2019-09-23 21:03:36 -0500

    Creating Base Project
    Adding favicon, index.html and app.js entry.
    Creatign Webpack dev and prod Settings.

---
+++ src/entries/app.js
@@ -0,0 +1,8 @@
+//@Author William E. Velázquez A. - info@williamvelazquez.com
+import React from "react";
+import { render } from "react-dom";
+import "babel-polyfill";
+
+const container = document.getElementById("app");
+
+render(<h1>Tech Events Schedule</h1>, container);
